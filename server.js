81b00e64168fc6a02c71342fbb4e7a01377a8282
Noa <noaisraeli3@gmail.com>
2021-05-26 13:51:17 +0300

    changes 26.5

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-/**
- * 'express' handles http requests with a cain of Middleware functions.
- * Every request that comes goes thorough a function that can 
- * return an answer or continues to the next function in chain. 
- */
+
 const express = require('express');
 const path = require('path');
 const port = 3000;
 
-// initializing new web server:
-const app = express();
+// initializing new web server
+const app = express(); 
+// Getting the data
+const data = require('./pokemon-data/pokemons.json')
+app.use(express.json());
+var pokemons_arr=[] // Array for pokemons objects
 
+app.get('/pokemons',(req,res)=>{
+    res.sendFile(path.resolve('pages/home_page.html'));
+});
+app.post('/pokemons', (req,res)=>{
+    for( p in data){
+        console.log(p)
+    }
 
+    
+});
 
-
-app.get('/',(req,res)=>res.sendFile(path.resolve('home_page.html')));
+app.get('/',(req,res)=>res.sendFile(path.resolve('pages/home_page.html')));
 app.listen(port);
