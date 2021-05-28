a027b7cedcd60af936b19c77b05ab317dd95b4f9
Noa <noaisraeli3@gmail.com>
2021-05-28 19:27:43 +0300

    finish

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ const port = 3000;
 const app = express();
 // Getting data 
 const data = require('./pokemons.json')
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 // Setting css styles and images as static
-app.use(express.static(path.join(__dirname,'public')))
-var pokArr=[];
+app.use(express.static(path.join(__dirname,'public')));
 var id_str = "";
 // Arranging data:
 for(i=0;i<data.length;i++){
@@ -21,16 +19,32 @@ for(i=0;i<data.length;i++){
     data[i].clicks = 0; // Adding counter for clicks
 }
 
-// Sorting the data and getting it from server
-app.get('/api/get_pokemons',(req,res)=>{
+// Returns html pages to the right paths
+app.get('/pokemons/:id',(req,res)=>{
+    var exact_pok=data.find((pok)=>pok.id==(req.params.id));
+    // Adding 1 to clicks counter:
+    exact_pok.clicks++;
+    res.sendFile(path.resolve('pages/pokemon_profile.html'));
+});
+app.get('/api/pokemons',(req,res)=>{
     data.sort((p1,p2)=>{
         return p1.id-p2.id;
     });
     res.send(data);
 });
-// Returns html page with the data
 app.get('/pokemons',(req,res)=>{
     res.sendFile(path.resolve('pages/all_pokemons.html'));
+});
+app.get('/api/pokemons/:id',(req,res)=>{
+    // Finding the exact pokemon for the given id in the path
+    var exact_pok=data.find((pok)=>pok.id==(req.params.id));
+    res.send(exact_pok);
+});
+app.get('/api/top3',(req,res)=>{
+    data.sort((p1,p2)=>{
+        return p2.clicks-p1.clicks;
+    });
+    res.send(data.slice(0,3));
 });
 app.get('/',(req,res)=>res.sendFile(path.resolve('pages/home_page.html')));
 app.listen(port);
